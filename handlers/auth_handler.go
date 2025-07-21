6dde3751532f361c35ddcbfbcdfbc1609aa897b9
Gabo <gabo.alucema@gmail.com>
2025-07-21 19:45:52 -0400

    handler ahora muestra el rol del usuario

--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -6,37 +6,48 @@ import (
 	"net/http"
 	"strings"
 
+	"backend-ventas/api/models"
+	"backend-ventas/services"
+
 	"github.com/gin-gonic/gin"
-	"backend-ventas/services" 
+	"gorm.io/gorm"
 )
 
-func VerifyToken(c *gin.Context) {
-	authHeader := c.GetHeader("Authorization")
-	if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Token no proporcionado"})
-		return
+func VerifyToken(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Token no proporcionado"})
+			return
+		}
+
+		idToken := strings.TrimPrefix(authHeader, "Bearer ")
+
+		// Verificar token con Firebase
+		token, err := services.FirebaseAuth.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
+			return
+		}
+
+		// Extraer información del token
+		email := token.Claims["email"].(string)
+		name := token.Claims["name"].(string)
+		uid := token.Claims["user_id"].(string)
+		picture := token.Claims["picture"].(string)
+
+		var usuario models.Usuario
+		if err := db.Preload("Rol").Where("email = ?", email).First(&usuario).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Usuario no registrado en el sistema"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"name":     name,
+			"email":    email,
+			"photoURL": picture,
+			"uid":      uid,
+			"rol":      usuario.Rol.Nombre,
+		})
 	}
-
-	idToken := strings.TrimPrefix(authHeader, "Bearer ")
-
-	// Verificar token con Firebase
-	token, err := services.FirebaseAuth.VerifyIDToken(context.Background(), idToken)
-	if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
-		return
-	}
-
-	// Extraer información del token
-	email := token.Claims["email"].(string)
-	name := token.Claims["name"].(string)
-	uid := token.Claims["user_id"].(string)
-	picture := token.Claims["picture"].(string)
-
-
-	c.JSON(http.StatusOK, gin.H{
-		"name":     name,
-		"email":    email,
-		"photoURL": picture,
-		"uid":      uid,
-	})
 }
